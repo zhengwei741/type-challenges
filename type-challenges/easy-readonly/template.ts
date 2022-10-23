ebfe4a05cbf7098a160eb94e8b3dbd004cf339fe
1217967340@qq.com <1217967340@qq.com>
2022-10-23 21:03:28 +0800

    easy-readonly

---
+++ type-challenges/easy-readonly/template.ts
@@ -0,0 +1,29 @@
+// 不要使用内置的Readonly<T>，自己实现一个。
+
+// 该 Readonly 会接收一个 泛型参数，并返回一个完全一样的类型，只是所有属性都会被 readonly 所修饰。
+
+// 也就是不可以再对该对象的属性赋值。
+
+// 例如：
+interface Todo {
+  readonly title: string
+  description: string
+}
+
+const todo: MyReadonly<Todo> = {
+ title: "Hey",
+  description: "foobar"
+}
+
+// todo.title = "Hello" // Error: cannot reassign a readonly property
+// todo.description = "barFoo" // Error: cannot reassign a readonly property
+
+type MyReadonly<T> = {
+  readonly [K in keyof T]: T[K]
+}
+
+// keyof获取T所有key
+// in 遍历 key
+// readonly 修饰key
+// T[K] 设置值
+
